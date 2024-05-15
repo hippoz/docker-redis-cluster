d8fd911ca83efc787dd5a4521d431b1465ee643b
EC2 Default User <ec2-user@ip-172-36-4-155.ap-northeast-2.compute.internal>
2024-05-15 07:58:14 +0000

    add sample script

---
+++ build-all-images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# example script to build the images
+
+docker buildx rm mybuilder
+
+docker buildx create --name mybuilder --use
+
+docker buildx inspect --bootstrap
+
+docker login --username jameszheng194
+
+cd docker-redis-cluster
+
+invoke build 7.2
+invoke build 7.0
+invoke build 6.2
+invoke build 6.0
+
+
+docker buildx rm mybuilder
+
+
